8f065dc307b39619dc6379004dd4f547f4f01ef6
Daniel Curtis <danielcurtis333@gmail.com>
2020-06-28 10:55:49 +1000

    added dht code

--- weather_station.ino
+++ weather_station.ino
@@ -27,20 +27,51 @@ test2
 // U8g2 Contructor List (Frame Buffer)
 // The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
 // Please update the pin numbers according to your setup. Use U8X8_PIN_NONE if the reset pin is not connected
-U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* E clock=*/ 2, /* R/W data=*/ 10, /* RS CS=*/ 7);
+
+//U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* E clock=*/ 13, /* R/W data=*/ 11, /* RS CS=*/ 10);
+
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8); // Look at Pins and change if not same as yours
+
+#include <dht.h> // Init for DHT 
+dht DHT;
+#define DHT11_PIN 7 // hooked up to pin 7
  
 void setup(void) {
   u8g2.begin();
+  Serial.begin(9600);
 }
 
 void display() {
+}
 
 void loop(void) {
-  u8g2.clearBuffer();					// clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB14_tr);	// choose a suitable font
-  u8g2.drawStr(0,20,"12:00");	// write something to the internal memory
-  u8g2.setFont(u8g2_font_unifont_t_symbols);
-  u8g2.drawUTF8(0,60,"☔");  // write something to the internal memory
-  u8g2.sendBuffer();					// transfer internal memory to the display
+
+  DHT.read11(DHT11_PIN); // Get DHT info and convert ( Maybe put in seperate void?)
+  
+int temps = DHT.temperature;
+char temp_reading_str[3];
+
+int tempsh = DHT.humidity;
+char temph_reading_str[3];
+
+   dtostrf(temps, 2, 0, temp_reading_str);
+   dtostrf(tempsh, 2, 0, temph_reading_str); // 
+
+   
+   u8g2.clearBuffer();					// clear the internal memory
+   u8g2.setFont(u8g2_font_ncenB14_tr);   // choose a suitable font
+   u8g2.drawStr(0, 20, temp_reading_str);  // write Temp to the internal memory
+   u8g2.setFont(u8g2_font_unifont_t_symbols);
+   u8g2.drawUTF8(30,20,"℃");
+   u8g2.drawStr(0, 40, temph_reading_str);  // write Humidity to the internal memory
+   u8g2.drawStr(20, 40, "%  Humidity");
+   u8g2.sendBuffer();                    // transfer internal memory to the display
   delay(1000);
+
+  Serial.print("Temperature = "); // had issue unless i sent to serial
+  Serial.println(DHT.temperature);
+  Serial.print("Humbillity = ");
+  Serial.println(DHT.humidity);
+  delay(1000);
+   
 }
