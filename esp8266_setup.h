49062f8746d33534e4c24d8f8a9dec42719c0340
Michael Pope <map7777@gmail.com>
2020-07-12 21:29:18 +1000

    ESP8266-FEAT-Add sendCommand function and connected

--- esp8266_setup.h
+++ esp8266_setup.h
@@ -12,6 +12,41 @@ String field = "field1";
 int countTrueCommand;
 int countTimeCommand; 
 boolean found = false; 
+boolean connected = false; 
 int valSensor = 1;
 
 SoftwareSerial esp8266(2, 3); // RX | TX
+
+void sendCommand(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while(countTimeCommand < (maxTime*1))
+  {
+    esp8266.println(command);//at+cipsend
+    if(esp8266.find(readReplay))//ok
+    {
+      found = true;
+      break;
+    }
+  
+    countTimeCommand++;
+  }
+  
+  if(found == true)
+  {
+    Serial.println("OYI");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+  
+  if(found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  
+  found = false;
+}
