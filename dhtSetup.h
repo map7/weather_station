1499efa90accccbd28e29ca15c87b628effd42c8
Michael Pope <map7777@gmail.com>
2020-07-12 21:02:28 +1000

    LCD-REFACTOR-Move DHT setup to separate file

---
+++ dhtSetup.h
@@ -0,0 +1,6 @@
+/* Temperature: Include and setup */
+#include <dht.h> // Init for DHT
+dht DHT;
+#define DHT11_PIN 10 // hooked up to pin 10 (blue)
+char temperature_str[2];
+char humidity_str[3];
--- weather_station.ino
+++ weather_station.ino
@@ -4,13 +4,7 @@
 
 #include <Arduino.h>
 #include "lcdSetup.h"
-
-/* Temperature: Include and setup */
-#include <dht.h> // Init for DHT
-dht DHT;
-#define DHT11_PIN 10 // hooked up to pin 10 (blue)
-char temperature_str[2];
-char humidity_str[3];
+#include "dhtSetup.h"
 
 /* RTC */
 #include "RTClib.h" // Rtc Lib
